3b513e6733700fe26f3b2afb8acb272bb4d1c64d
Mario <mario.guerra.vaz@gmail.com>
2026-02-17 11:09:45 +0100

    Lo pasamos a mi casa

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,18 @@ import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
   host: "localhost",
-  user: "dwes25",
-  password: "dwes",
+  user: "root",
+  password: "",
   database: "examen_dwes_3",
 });
+
+//
+//import mysql from "mysql2/promise";
+//
+//export const db = mysql.createPool({
+//  host: "localhost",
+//  user: "dwes25",
+//  password: "dwes",
+//  database: "examen_dwes_3",
+//});
+
